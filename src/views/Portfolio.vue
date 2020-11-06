f44254f7016bd758cc8c547f7cd01e470dda3e06
Andrew Andrijchuk <andrew.andrijchuk@wpt.dev>
2020-11-07 00:17:13 +0200

    add page 'my works'

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,28 +2,35 @@
   <div class="portfolio">
     <h2 class="title">My Work</h2>
     <div class="portfolio-list">
-      <details class="portfolio-item">
-        <summary>Верстка</summary>
-        <p>Приклад №1</p>
-        <p>Приклад №2</p>
-        <p>Приклад №3</p>
-      </details>
-      <details class="portfolio-item">
-        <summary>Vue.js</summary>
-        <p>Приклад №1</p>
-        <p>Приклад №2</p>
-      </details>
-      <details class="portfolio-item">
-        <summary>React</summary>
-        <p>Приклад №1</p>
-      </details>
+      <div
+        class="portfolio-item"
+        v-for="(item, index) of listTechnologies"
+        :key="index"
+        :style="{
+          background: `url(${require(`@/assets/images/${item.picture}`)})center center no-repeat`
+        }"
+      >
+        <span class="portfolio-item--desc">
+          Кількість проектів на {{ item.technology }}:
+          {{ item.numberProjects }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Portfolio"
+  name: "Portfolio",
+  data() {
+    return {
+      listTechnologies: [
+        { technology: "Vue", numberProjects: 0, picture: "vue.svg" },
+        { technology: "React", numberProjects: 0, picture: "react.svg" },
+        { technology: "JS", numberProjects: 0, picture: "javascript.svg" }
+      ]
+    };
+  }
 };
 </script>
 
